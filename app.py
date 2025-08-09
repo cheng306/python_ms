5a6fd402ef43af3a82c111437b7cd7a87ec9f18f
Johnny Cheng <johnnycheng@microsoft.com>
2025-08-08 18:12:30 -0700

    testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({"message": "hello Johnny"})
+    return jsonify({"message": "hello Johnnyy"})
 
 @app.route('/health')
 def health():
